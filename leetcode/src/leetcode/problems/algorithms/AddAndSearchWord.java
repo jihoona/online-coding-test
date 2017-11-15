aa98cc74527971b35cb545d9a27d11ba2e67c4fc
Jihoon Ko <jhko@outlook.com>
2017-11-16 00:25:35 +0900

    Add the answer for Add and Search Word problem.

---
+++ leetcode/src/leetcode/problems/algorithms/AddAndSearchWord.java
@@ -0,0 +1,69 @@
+package leetcode.problems.algorithms;
+
+public class AddAndSearchWord {
+
+	/*
+    *   211. Add and Search Word - Data structure design
+    *
+    *   Input: addWord("bad")
+    *          addWord("dad")
+    *          addWord("mad")
+    *   Process: Search(word) can search a literal word or a regular expression string
+    *            containing only letters a-z or .
+    *   Output: search("pad") -> false
+    *           search("bad") -> true
+    *           search(".ad") -> true
+    *           search("b..") -> true
+    *
+    *   Requirement:
+    *
+    *   Personal review: The code below is taken from https://discuss.leetcode.com/topic/14030/my-simple-and-clean-java-code/4.
+    *                    Trie (https://en.wikipedia.org/wiki/Trie)
+    */
+
+    public class TrieNode {
+        public TrieNode[] children = new TrieNode[26];
+        public boolean isWord;
+    }
+
+    private TrieNode root = new TrieNode();
+
+    // Adds a word into the data structure.
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+
+    // Returns if the word is in the data structure. A word could
+    // contain the dot character '.' to represent any one letter.
+    public boolean search(String word) {
+        return match(word.toCharArray(), 0, root);
+    }
+
+    private boolean match(char[] chs, int k, TrieNode node) {
+        if (k == chs.length) {
+            return node.isWord;
+        }
+        if (chs[k] == '.') {
+            for (int i = 0; i < node.children.length; i++) {
+                if (node.children[i] != null && match(chs, k + 1, node.children[i])) {
+                    return true;
+                }
+            }
+        } else {
+            return node.children[chs[k] - 'a'] != null && match(chs, k + 1, node.children[chs[k] - 'a']);
+        }
+        return false;
+    }
+}
+
+// Your WordDictionary object will be instantiated and called as such:
+// WordDictionary wordDictionary = new WordDictionary();
+// wordDictionary.addWord("word");
+// wordDictionary.search("pattern");
